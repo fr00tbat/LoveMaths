b811d91fd7e5f4bcf1a31162b8dc84b3f5858615
fr00tbat <fruitbat@fruitbat.ie>
2023-03-17 21:11:34 +0000

    Add subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,8 +46,8 @@ function runGame(gameType) {
 
     if(gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    // } else if(gameType === "subtract") {
-    //     displaySubtractQuestion(num1, num2);
+    } else if(gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
     } else if(gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
     // } else if(gameType === "division") {
@@ -90,8 +90,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    // } else if (operator === "-") {
-    //     return [operand1 - operand2, "subtract"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
     // } else if (operator === "/") {
@@ -167,13 +167,25 @@ function displayAdditionQuestion(operand1, operand2) {
     
 }
 
-// function displaySubtractQuestion(operand1, operand2) {
+function displaySubtractQuestion(operand1, operand2) {
 
-//     document.getElementById('operand1').textContent = operand1;
-//     document.getElementById('operand2').textContent = operand2;
-//     document.getElementById('operator').textContent = "-";
+    //only allow the larger of the nums to appear first
+    if(operand2 > operand1){
+        let tmp = operand1;
+        operand1 = operand2;
+        operand2 = tmp;
+    }
 
-// }
+    /**ternary condition version (replace below):
+     * document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+     * document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1; 
+     */
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "-";
+
+}
 
 function displayMultiplyQuestion(operand1, operand2) {
 
